7cd2dfe664a07ff546bb62c2331a3fdb56bf67a4
Kevin Liao <kevinliao@lawn-143-215-52-24.lawn.gatech.edu>
2016-09-06 12:38:54 -0400

    Implementing instance variables and scanner

--- Civilization.java
+++ Civilization.java
@@ -1,14 +1,39 @@
+import java.util.Scanner;
+
 public class Civilization {
     static boolean playing = true;
+    private String[] cityArray;
+    private int numAttacks;
+    private double gold;
+    private double resources;
+    private int happiness;
+    private int military;
+    private int techPoints;
+
+    public Civilization(String[] city, int attacks, double gold,
+                        double resources, int happiness, int military,
+                        int tech) {
+        city = new String[5];
+        cityArray = city;
+        numAttacks = attacks;
+        gold = gold;
+        resources = resources;
+        happiness = happiness;
+        military = military;
+        techPoints = tech;
+    }
 
     public static void main(String[] args) {
+        Scanner keyboard = new Scanner(System.in);
+        System.out.println("Which civilization would you like to lead? Choose a number:\n1. American (George Washington)\n2. Zulu (Shaka)\n3. English (Queen Elizabeth 1)\n4. Chinese (Wu Zetian)" );
+        int civ = keyboard.nextInt();
+        while (playing) {
 
-        while(playing) {
-            //Your game code here
-
-            //Use this to break out of the game loop.
-            //Feel free to move it around.
             playing = false;
         }
+    }
+
+    public void turn() {
+
     }
 }
