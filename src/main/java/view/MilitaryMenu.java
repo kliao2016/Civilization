9247e25a35145f5aad8a122ea07d1ed2bf816f95
Kevin Liao <kliao2020@gatech.edu>
2016-11-25 20:26:45 -0700

    fixed militarymenu

--- src/main/java/view/MilitaryMenu.java
+++ src/main/java/view/MilitaryMenu.java
@@ -2,6 +2,8 @@ package view;
 
 import controller.GameController;
 import javafx.scene.control.Button;
+import model.MapObject;
+import model.MilitaryUnit;
 
 /**
  * Created by William on 11/11/2016.
@@ -17,18 +19,23 @@ public class MilitaryMenu extends AbstractMenu {
     */
     public MilitaryMenu() {
         attackButton.setOnMousePressed(e -> {
+                MapObject occup = GameController.getLastClicked().getTile()
+                                                                 .getOccupant();
                 GameController.attacking();
-                if (GameController.getGameState()
-                    != GameController.GameState.NEUTRAL) {
+                if (occup.isMilitaryUnit()
+                    && !((MilitaryUnit) occup).getCanAttack()){
                     NoActionAlert.displayAlert();
                 }
                 GameController.updateResourcesBar();
             });
 
         moveButton.setOnMousePressed(e -> {
+                MapObject occup = GameController.getLastClicked().getTile()
+                                                                 .getOccupant();
                 GameController.moving();
-                if (GameController.getGameState()
-                    != GameController.GameState.NEUTRAL) {
+                if (occup.isMilitaryUnit()
+                    && !((MilitaryUnit) occup)
+                    .canMove(((MilitaryUnit) occup).getBaseEndurance())) {
                     NoActionAlert.displayAlert();
                 }
             });
